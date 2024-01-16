39bc2615d9c64f4098627de885e6b6c1de1901e3
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-01-16 14:48:51 +0530

    setting up

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+    return<div>
+      Hello home page
+    </div>
+}
+
+export default Home;
